bd0966de084ad1eef946f4b05e5b25a3c793285e
justintimejt <justinchow3607@gmail.com>
2025-01-18 23:47:10 -0500

    bug fixes

--- flask_app/static/scripts.js
+++ flask_app/static/scripts.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function sortResults(results, sortBy) {
         //sort by sentiments
         if (sortBy === 'sentiment') {
-            return results.sort((b,a) => {
+            return results.sort((a,b) => {
                 const sentimentA = Math.round((a.score[0] + 1) * 50);
                 const sentimentB = Math.round((b.score[0] + 1) * 50);
                 return sentimentA - sentimentB;
